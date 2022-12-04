7d91b2072fa5f846c79896154e4accb9d8eaa15c
LipezJ <juandavidlipez47@gmail.com>
2022-12-04 16:15:32 -0500

    curses

--- pong-guest.py
+++ pong-guest.py
@@ -1,69 +1,88 @@
-#librerias
-from pytimedinput import timedKey
 import random as rn
 import os
 import time
+import curses
 import requests
+from curses import wrapper
+from pytimedinput import timedKey
 
-#variables
-tablero = [[0 for b in range(40)] for a in range(12)]
-area2 = [0, 0, 0, 0, 0, '|', '|', 0, 0, 0, 0, 0]
+def imprimir_tablero(stdscr, nuevo, actual_rows1, actual_rows2,puntos):
+    stdscr.addstr('x'*(tablero[1]+1) + f'\n')
+    for row in range(tablero[0]):
+        if row == actual_rows1[0] or row == actual_rows1[1]:
+            stdscr.addstr('|')
+        else:
+            stdscr.addstr(' ')
+        if nuevo[0] == row:
+            stdscr.addstr((' '*(nuevo[1]-1)) + 'O' + ' '*(tablero[1]-(nuevo[1]+1)))
+        else:
+            stdscr.addstr(' '*(tablero[1]-1))
+        if row == actual_rows2[0] or row == actual_rows2[1]:
+            stdscr.addstr('|')
+        else:
+            stdscr.addstr(' ')
+        stdscr.addstr(f'\n')
+    stdscr.addstr('x'*(tablero[1]+1))
+    stdscr.addstr(f'\n {puntos[0]} - {puntos[1]}')
 
-def imprimir():
-    os.system('cls')
-    for i in range(len(tablero[0])+1):
-        print('x', end='')
-    print()
-    for i in range(len(area2)):
+def posibilidades_(actual):
+    if (actual[0] == 0 and actual[1] == 0) or (actual[0] == 0 and actual[1] == tablero[1]-1) or (actual[0] == tablero[0]-1 and actual[1] == 0) or (actual[0] == tablero[0]-1 and actual[1] == tablero[1]-1):
+        return [4]
+    if actual[0] == tablero[0]-1:
+        posibilidades_ = [0, 1]
+    elif actual[0] == 0:
+        posibilidades_ = [2, 3]
+    elif actual[1] == tablero[1]-1:
+        posibilidades_ = [0, 3]
+    elif actual[1] == 0:
+        posibilidades_ = [1, 2]
+    else:
+        posibilidades_ = [0,1,2,3]
+    return posibilidades_
+
+tablero = [15, 50]
+rows = tablero[0]
+movimientos = {
+    0: [-1, -1], 1: [-1, 1], 
+    2: [1, 1], 3: [1, -1]
+}
+movimientos_ = {
+    "-1-1": 0, "-11": 1,
+    "1-1": 3, "11": 2,
+    "00": 4
+}
+movimientos__ = {0: 2, 1: 3, 2: 0, 3: 1}
+movimientos_area = {'k': -1, 'm': 1}
+
+party = input('igrese el nombre de la party: ')
+os.system(f'mode con: cols={tablero[1]+3} lines={tablero[0]+4}')
+
+def main(stdscr):
+
+    actual_rows2 = [rows//2, rows//2+1]
+    puntos = [0, 0]
+
+    while True:
+        stdscr.clear()
+
+        #teclas
+        key, timeout = timedKey(allowCharacters='qkm', timeout=0.1, toprint=False)
+        if key == 'q':
+            print(exit())
+
+        if not timeout:
+            if (key == 'k' and actual_rows2[0]) > 0 or (key == 'm' and actual_rows2[1] < rows-1):
+                actual_rows2 = [ actual_rows2[0] + movimientos_area[key], actual_rows2[1] + movimientos_area[key]]
+                requests.post(f'http://localhost:3000/setg/{party}/{actual_rows2[1]}/', data={})
+
+        if 10 in puntos:
+            if puntos[0] == 10: print(f'\n Ganaste! jugador 1 \n')
+            else: print(f'\n Ganaste! jugador 2 \n')
+            time.sleep(1)
+            print(exit())
+
         response = requests.get(f'http://localhost:3000/get/{party}', data={}).json()
-        if i == response[0] or i+1 == response[0]:
-            print('|', end='')
-        else:
-            print(' ', end='')
-        for j in range(len(tablero[0])):
-            if i == response[1][0] and j == response[1][1]:
-                print('O', end='')
-            elif j == centro:
-                print('|', end='')
-            else:
-                print(' ', end='')
-        if area2[i] == '|':
-            print(area2[i], end='')
-            requests.post(f'http://localhost:3000/setg/{party}/{i}', data={})
-        else:
-            print(' ', end='')
-        print()
-    for i in range(len(tablero[0])+1):
-        print('x', end='')
-    print()
-    print('puntos: ', puntos, ' - ', puntos2)
+        imprimir_tablero(stdscr, response[1], [response[0], response[0]+1], actual_rows2, puntos)
+        stdscr.refresh()
 
-def mover2():
-    nuevo_area2 = [
-        actual_area2[0] + movimientos_area[key], 
-        actual_area2[1] + movimientos_area[key]
-    ]
-    area2[actual_area2[0]] = 0
-    area2[actual_area2[1]] = 0  
-    area2[nuevo_area2[0]] = '|'
-    area2[nuevo_area2[1]] = '|'
-
-os.system("mode con: cols=42 lines=20")
-centro = (len(tablero[0])-1)//2
-movimientos_area = {'w': -1, 'd': 1, 'k': -1, 'm': 1}
-
-puntos, puntos2 = 0, 0
-party = input("ingrese el nombre de la party: ")
-
-while(True):
-    actual_area2 = [area2.index('|')]
-    actual_area2.append(actual_area2[0]+1)
-    key, timeout = timedKey(allowCharacters='qkm', timeout=0.1)
-    if key == 'q':
-        break
-    if not timeout:
-        if (key == 'k' and actual_area2[0] > 0) or (key == 'm' and actual_area2[1] < len(area2)-1):
-            mover2()
-        time.sleep(0.05)
-    time.sleep(0.02)
-    imprimir()
+wrapper(main)
--- pong-host.py
+++ pong-host.py
@@ -1,87 +1,38 @@
-#librerias
-from pytimedinput import timedKey
 import random as rn
 import os
 import time
+import curses
 import requests
+from curses import wrapper
+from pytimedinput import timedKey
 
-#variables
-#   generales
-
-tablero = [[0 for b in range(40)] for a in range(12)]
-area = [0, 0, 0, 0, 0, '|', '|', 0, 0, 0, 0, 0]
-area2 = [0, 0, 0, 0, 0, '|', '|', 0, 0, 0, 0, 0]
-#   tablero
-movimientos = {
-    0: [-1, -1], 1: [-1, 1], 
-    2: [1, 1], 3: [1, -1]
-}
-movimientos_ = {
-    '-1-1': 0, '-11': 1,
-    '1-1': 3, '11': 2,
-    '00': 4
-}
-movimientos__ = {0: 2, 1: 3, 2: 0, 3: 1}
-#   area
-movimientos_area = {'w': -1, 'd': 1, 'k': -1, 'm': 1}
-
-
-#funciones generales
-#   imprimir
-def imprimir():
-    os.system('cls')
-    for i in range(len(tablero[0])+1):
-        print('x', end='')
-    print()
-    for i in range(len(area)):
-        response = requests.get(f'http://localhost:3000/get/{party}', data={}).json()
-        if area[i] == '|':
-            print(area[i], end='')
-            requests.post(f'http://localhost:3000/sethr/{party}/{i}', data={})
+def imprimir_tablero(stdscr, nuevo, actual_rows1, actual_rows2,puntos):
+    stdscr.addstr('x'*(tablero[1]+1) + f'\n')
+    for row in range(tablero[0]):
+        if row == actual_rows1[0] or row == actual_rows1[1]:
+            stdscr.addstr('|')
         else:
-            print(' ', end='')
-
-        for j in range(len(tablero[0])):
-            if tablero[i][j] == 1:
-                print('O', end='')
-                requests.post(f'http://localhost:3000/setht/{party}/{i},{j}', data={})
-            elif j == centro:
-                print('|', end='')
-            else:
-                print(' ', end='')
-        if i == response[2]-1 or i == response[2]:
-            print('|', end='')
+            stdscr.addstr(' ')
+        if nuevo[0] == row:
+            stdscr.addstr((' '*(nuevo[1])) + 'O' + ' '*(tablero[1]-(nuevo[1]+1)))
         else:
-            print(' ', end='')
-        print()
-    for i in range(len(tablero[0])+1):
-        print('x', end='')
-    print()
-    print('puntos: ', puntos, ' - ', puntos2)
-#   elegir una nueva caciila
-def random_():
-    tablero[actual[0]][actual[1]] = 0
-    inicio_fila = rn.randint(0, len(tablero)-1)
-    inicio_columna = (len(tablero[0])-1)//2
+            stdscr.addstr(' '*(tablero[1]))
+        if row == actual_rows2[0] or row == actual_rows2[1]:
+            stdscr.addstr('|')
+        else:
+            stdscr.addstr(' ')
+        stdscr.addstr(f'\n')
+    stdscr.addstr('x'*(tablero[1]+1))
+    stdscr.addstr(f'\n {puntos[0]} - {puntos[1]}')
 
-    tablero[inicio_fila][inicio_columna] = 1
-    return [inicio_fila, inicio_columna]
-
-#funciones para el tablero
-#   buscar la "pelota"
-def buscar_uno():
-    for i in range(len(tablero)):
-        if 1 in tablero[i]:
-            return [i, tablero[i].index(1)]
-#   calcular las posibilidades
-def posibilidades_():
-    if (actual[0] == 0 and actual[1] == 0) or (actual[0] == 0 and actual[1] == len(tablero[0])-1) or (actual[0] == len(tablero)-1 and actual[1] == 0) or (actual[0] == len(tablero)-1 and actual[1] == len(tablero[0])-1):
+def posibilidades_(actual):
+    if (actual[0] == 0 and actual[1] == 0) or (actual[0] == 0 and actual[1] == tablero[1]-1) or (actual[0] == tablero[0]-1 and actual[1] == 0) or (actual[0] == tablero[0]-1 and actual[1] == tablero[1]-1):
         return [4]
-    if actual[0] == len(tablero)-1:
+    if actual[0] == tablero[0]-1:
         posibilidades_ = [0, 1]
     elif actual[0] == 0:
         posibilidades_ = [2, 3]
-    elif actual[1] == len(tablero[0])-1:
+    elif actual[1] == tablero[1]-1:
         posibilidades_ = [0, 3]
     elif actual[1] == 0:
         posibilidades_ = [1, 2]
@@ -89,138 +40,107 @@ def posibilidades_():
         posibilidades_ = [0,1,2,3]
     return posibilidades_
 
-#funciones para el area de raqueta
-#   movimientos
-def mover():
-    nuevo_area = [
-        actual_area[0] + movimientos_area[key], 
-        actual_area[1] + movimientos_area[key]
-   ]
-    area[actual_area[0]] = 0
-    area[actual_area[1]] = 0  
-    area[nuevo_area[0]] = '|'
-    area[nuevo_area[1]] = '|'
+tablero = [15, 50]
+rows = tablero[0]
+movimientos = {
+    0: [-1, -1], 1: [-1, 1], 
+    2: [1, 1], 3: [1, -1]
+}
+movimientos_ = {
+    "-1-1": 0, "-11": 1,
+    "1-1": 3, "11": 2,
+    "00": 4
+}
+movimientos__ = {0: 2, 1: 3, 2: 0, 3: 1}
+movimientos_area = {'w': -1, 'd': 1}
 
-# codigo
+party = input('igrese el nombre de la party: ')
+os.system(f'mode con: cols={tablero[1]+3} lines={tablero[0]+4}')
 
-#inicializando variables
-os.system("mode con: cols=42 lines=20")
-centro = (len(tablero[0])-1)//2
-inicio_fila = rn.randint(0, len(tablero)-1)
+def main(stdscr):
 
-tablero[inicio_fila][centro] = 1
-actual = [inicio_fila, centro]
+    actual_rows1 = [rows//2, rows//2+1]
+    actual_rows2 = [rows//2, rows//2+1]
+    nuevo = [rn.randint(0, tablero[0]-1), tablero[1]//2]
+    actual = nuevo
+    puntos = [0, 0]
 
-puntos, puntos2 = 0, 0
-party = input("ingrese el nombre de la party: ")
+    while True:
+        stdscr.clear()
 
-#bucle general
-while True:
+        #teclas
+        key, timeout = timedKey(allowCharacters='wdq', timeout=0.1, toprint=False)
+        if key == 'q':
+            print(exit())
 
-    #calcular posicion de la raqueta
-    actual_area = [area.index('|')]
-    actual_area.append(actual_area[0]+1)
-    response = requests.get(f'http://localhost:3000/get/{party}', data={}).json()
-    actual_area2 = [response[2]-1, response[2]]
-    #calcular posicion de la pelota
-    anterior = actual   
-    actual = buscar_uno()
+        anterior = actual
+        actual = nuevo
 
-    #calcular el tipo de movimiento anterior
-    tipo = str(actual[0] - anterior[0]) + str(actual[1] - anterior[1])
-    tipo = movimientos_[tipo]
-    #ejecutar la funcion para saber cuantas posibilidades hay en el movimiento
-    posibilidades = posibilidades_()
+        #calcular el tipo de movimiento anterior
+        tipo = movimientos_[str(actual[0] - anterior[0]) + str(actual[1] - anterior[1])]
+        #ejecutar la funcion para saber cuantas posibilidades hay en el movimiento
+        posibilidades = posibilidades_(actual)
 
-    #escucha de teclas
-    key, timeout = timedKey(allowCharacters='wdq', timeout=0.1)
+        if not timeout:
+            if (key == 'w' and actual_rows1[0]) > 0 or (key == 'd' and actual_rows1[1] < rows-1):
+                actual_rows1 = [ actual_rows1[0] + movimientos_area[key], actual_rows1[1] + movimientos_area[key]]
 
-    #   area
-    #si se presiona 'q' se saldra del programa
-    if key == 'q':
-        print(exit())
-    
-    #puntos
-    if puntos == 10:
-        os.system('cls')
-        print('Ganaste! jugador 1')
-        time.sleep(1.5)
-        print(exit())
-    elif puntos2 == 10:
-        os.system('cls')
-        print('Ganaste! jugador 2')
-        time.sleep(1.5)
-        print(exit())
+        if 10 in puntos:
+            if puntos[0] == 10: print(f'\n Ganaste! jugador 1 \n')
+            else: print(f'\n Ganaste! jugador 2 \n')
+            time.sleep(1)
+            print(exit())
+            
+        #primer movimiento
+        if actual == anterior:
+            posibilidad = rn.choice(posibilidades)
+            nuevo = [actual[0] + (movimientos[posibilidad][0]), actual[1] + (movimientos[posibilidad][0])]
+        #esquinas
+        elif posibilidades[0] == 4:
+            if  actual[0] == actual_rows1[0] or actual[0] == actual_rows1[1] or actual_rows2[0] == actual[0] or actual_rows2[1] == actual[0]:
+                nuevo = anterior
+            elif not (actual_rows1[0] == actual[0] or actual_rows1[1] == actual[0]):
+                nuevo = [rn.randint(0, tablero[0]-1), tablero[1]//2]
+                actual = nuevo
+                puntos[1] += 1
+            elif not (actual_rows2[0] == actual[0] or actual_rows2[1] == actual[0]):
+                nuevo = [rn.randint(0, tablero[0]-1), tablero[1]//2]
+                actual = nuevo
+                puntos[0] += 1
+        #movimientto normal
+        elif len(posibilidades) == 4:
+            nuevo = [actual[0] + (actual[0] - anterior[0]), actual[1] + (actual[1] - anterior[1])]
 
-    #si se presiona una tecla procede a evaluar el movimiento
-    if not timeout:
-        #movimiento 1
-        if (key == 'w' and actual_area[0] > 0) or (key == 'd' and actual_area[1] < len(area)-1):
-            mover()
+        elif posibilidades == [1, 2] and not (actual_rows1[0] == actual[0] or actual_rows1[1] == actual[0]):
+            nuevo = [rn.randint(0, tablero[0]-1), tablero[1]//2]
+            actual = nuevo
+            puntos[1] += 1
+        elif posibilidades == [0, 3] and not (actual_rows2[0] == actual[0] or actual_rows2[1] == actual[0]):
+            nuevo = [rn.randint(0, tablero[0]-1), tablero[1]//2]
+            actual = nuevo
+            puntos[0] += 1
+
+        #rebotes
+        else:
+            if posibilidades == [1, 2]:
+                if actual_rows1[0] == actual[0]:
+                    tipo = 0
+                elif actual_rows1[1] == actual[0]:
+                    tipo = 3
+            if posibilidades == [0, 3]:
+                if actual_rows2[0] == actual[0]:
+                    tipo = 1
+                elif actual_rows2[1] == actual[0]:
+                    tipo = 2
+            posibilidades.remove(movimientos__[tipo])
+            nuevo = [actual[0] + movimientos[posibilidades[0]][0], actual[1] + movimientos[posibilidades[0]][1]]
         
+        requests.post(f'http://localhost:3000/setht/{party}/{actual[0]},{actual[1]}/', data={})
+        requests.post(f'http://localhost:3000/sethr/{party}/{actual_rows1[0]}/', data={})
+        response = requests.get(f'http://localhost:3000/get/{party}', data={}).json()
+        actual_rows2 = [response[2], response[2]-1]
 
-    # volver a calcular la posicion de la raqueta para los rebotes
-    actual_area = [area.index('|')]
-    actual_area.append(actual_area[0]+1)
-    response = requests.get(f'http://localhost:3000/get/{party}', data={}).json()
-    actual_area2 = [response[2]-1, response[2]]
+        imprimir_tablero(stdscr, nuevo, actual_rows1, actual_rows2, puntos)
+        stdscr.refresh()
 
-    #   tablero
-    #primer movimiento
-    if actual == anterior:
-        posibilidades = rn.choice(posibilidades_())
-        nuevo = movimientos[posibilidades]
-        nuevo = [actual[0] + nuevo[0], actual[1] + nuevo[1]]
-    
-    #esquinas
-    elif posibilidades[0] == 4:
-        if actual[0] == actual_area[0] or actual[0] == actual_area[0] or actual_area[0] == actual[0] or actual_area[1] == actual[0]:
-            nuevo = anterior
-        elif not (actual_area[0] == actual[0] or actual_area[1] == actual[0]):
-            actual = random_()
-            puntos2 += 1
-            time.sleep(0.5)
-            continue
-        elif not (actual_area2[0] == actual[0] or actual_area2[1] == actual[0]):
-            actual = random_()
-            puntos += 1
-            time.sleep(0.5)
-            continue
-
-
-    #movimientto normal
-    elif len(posibilidades) == 4:
-        nuevo = [actual[0] - anterior[0], actual[1] - anterior[1]]
-        nuevo = [actual[0] + nuevo[0], actual[1] + nuevo[1]]
-    
-    #rebotes
-    elif posibilidades == [1, 2] and not (actual_area[0] == actual[0] or actual_area[1] == actual[0]):
-        actual = random_()
-        puntos2 += 1
-        time.sleep(0.5)
-        continue
-    elif posibilidades == [0, 3] and not (actual_area2[0] == actual[0] or actual_area2[1] == actual[0]):
-        actual = random_()
-        puntos += 1
-        time.sleep(0.5)
-        continue
-
-    else:
-        if posibilidades == [1, 2]:
-            if actual_area[0] == actual[0]:
-                tipo = 0
-            elif actual_area[1] == actual[0]:
-                tipo = 3
-        if posibilidades == [0, 3]:
-            if actual_area2[0] == actual[0]:
-                tipo = 1
-            elif actual_area2[1] == actual[0]:
-                tipo = 2
-        tipo_ = movimientos__[tipo]
-        posibilidades.remove(tipo_)
-        nuevo = movimientos[posibilidades[0]]
-        nuevo = [actual[0] + nuevo[0], actual[1] + nuevo[1]]
-
-    tablero[nuevo[0]][nuevo[1]] = 1
-    tablero[actual[0]][actual[1]] = 0
-    imprimir()
+wrapper(main)
